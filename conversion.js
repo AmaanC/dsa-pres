a705eb529d576006902a792413e98ab2dce505ae
Amaan Cheval <amaan.cheval@gmail.com>
2015-09-26 17:09:44 +0530

    Added step-by-step visualizer

--- conversion.js
+++ conversion.js
@@ -1,8 +1,23 @@
 (function() {
+    var form = document.getElementById('form');
     var infixElem = document.getElementById('infix');
-    var postfixElem = document.getElementById('postfix');
     var infixExp = '';
     var stack = [];
+
+    var canvas = document.getElementById('postfix');
+    var ctx = canvas.getContext('2d');
+
+    var distBet;
+    var heightOffset;
+
+    var DEFAULT_FONT = '20px Arial';
+    var FONT_SIZE = parseInt(DEFAULT_FONT, 10);
+
+    var DEFAULT_COLOR = 'black';
+    var CHAR_COLOR = 'red';
+
+    var START_HEIGHT = 30;
+    var currentHeight;
 
     var isOperand = function(c) {
         return /[0-9a-zA-Z]/.test(c);
@@ -61,7 +76,6 @@
                 }
                 else if (stack.length === 0 || seeTop(stack) === '(' || higherPrecedence(c, seeTop(stack))) {
                     stack.push(c);
-                    console.log('Pushing', c);
                 }
                 else {
                     while (stack.length > 0) {
@@ -71,16 +85,48 @@
                     stack.push(c);
                 }
             }
+
+            saveStep(c, postExp, stack);
         }
         while (stack.length > 0) {
             temp = stack.pop();
             postExp += temp;
         }
+        saveStep('\\0', postExp, stack);
         return postExp;
     };
 
-    infixElem.addEventListener('input', function() {
-        infixExp = this.value;
-        postfixElem.textContent = convert(infixExp);
-    }, false);
+    var saveStep = function(c, postExp, stack) {
+        ctx.fillStyle = CHAR_COLOR;
+        ctx.fillText('Current character: \'' + c + '\'', 0, currentHeight);
+        ctx.fillStyle = DEFAULT_COLOR;
+
+        currentHeight += distBet;
+        ctx.fillText('Stack: [\'' + stack.join('\', \'') + '\']', 0, currentHeight);
+        currentHeight += distBet;
+        ctx.fillText('Postfix expression: ' + postExp, 0, currentHeight);
+
+        currentHeight += heightOffset;
+    };
+
+    var generate = function(e) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        currentHeight = START_HEIGHT;
+        distBet = FONT_SIZE;
+        heightOffset = 2 * FONT_SIZE;
+
+        infixExp = infixElem.value;
+        canvas.height = infixExp.length * FONT_SIZE * 6;
+
+        ctx.font = DEFAULT_FONT;
+
+        convert(infixExp);
+        if (e) {
+            e.preventDefault();
+        }
+    };
+
+    form.addEventListener('submit', generate, false);
+    infixElem.focus();
+    generate();
 })();
